a072da3911aa52ce9f26354d83fa8d7b5d547bdb
raya <fati.mansoory@gmail.com>
2023-12-14 18:30:11 +0330

    create blog.models.py

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    image = models.ImageField(null=True, blank=True)
+    category = models.ForeignKey(
+        'Category', on_delete=models.SET_NULL, null=True)
+    # tags = TaggableManager()
+    # counted_views = models.IntegerField(null=True, default=0)
+    status = models.BooleanField(default=False,)
+    # login_require = models.BooleanField(default=False)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
