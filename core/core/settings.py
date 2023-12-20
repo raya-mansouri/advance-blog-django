b3cd90c085e1aacd5200973e80bee50c5168875d
raya <fati.mansoory@gmail.com>
2023-12-20 12:35:15 +0330

    create AbstractBaseUser

--- core/core/settings.py
+++ core/core/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'accounts',
-    'blog',
+    # 'blog',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# user manager config
+AUTH_USER_MODEL = "accounts.User"
