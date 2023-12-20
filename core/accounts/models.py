99d64fce29f3712de17d15771652f0838c8e8dff
raya <fati.mansoory@gmail.com>
2023-12-20 21:33:09 +0330

    use signals to create a profile after createing each user

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser, PermissionsMixin)
 from django.db.models.fields.related import ForeignKey
 # from django.utils.translation import ugettext_lazy as _
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 
@@ -75,3 +75,8 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.email
+    
+@receiver(post_save,sender=User)
+def save_profile(sender,instance,created,**kwargs):
+    if created:
+        Profile.objects.create(user=instance)
