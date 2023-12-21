babb3ad8e9ad345cc52fe5681c26306485a82989
raya <fati.mansoory@gmail.com>
2023-12-21 12:49:30 +0330

    RedirectView

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 from .views import *
 
-name = 'blog'
+app_name = 'blog'
 
 urlpatterns = [
-path("", IndexView.as_view(), name="home"),]
+    path("", IndexView.as_view(), name="index"),
+    path(
+        "go-to-index/",
+        RedirectView.as_view(pattern_name='blog:index'),
+        name="go-to-index",
+    ),
+]
