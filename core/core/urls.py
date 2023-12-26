fa2a283ab2091e249c92d198c5ea586293cdd4be
raya <fati.mansoory@gmail.com>
2023-12-26 21:47:02 +0330

    put auth option in DRF template

--- core/core/urls.py
+++ core/core/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('blog/', include('blog.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
 
 if settings.DEBUG:
