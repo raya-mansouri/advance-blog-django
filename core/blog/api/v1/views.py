a9f26d03f0d7df5862bb14e290cd119b6305175a
raya <fati.mansoory@gmail.com>
2023-12-26 21:43:19 +0330

    use permission for  @api_view decorator with function based views

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import PostSerializer
@@ -20,6 +21,7 @@ def post_list(request):
 
 
 @api_view(['GET','PUT','DELETE'])
+@permission_classes([IsAuthenticatedOrReadOnly])
 def post_detail(request,id):
     post = get_object_or_404(Post, pk=id, status= True)
     if request.method == 'GET':
