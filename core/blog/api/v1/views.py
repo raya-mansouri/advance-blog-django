597f69e9ddbb0f312745c58afdb252f840acaa64
raya <fati.mansoory@gmail.com>
2023-12-28 17:24:50 +0330

    replace fbv with cbv for api format of PostDetail

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -8,19 +8,6 @@ from .serializers import PostSerializer
 from blog.models import Post
 
 
-# @api_view(['GET', 'POST'])
-# def post_list(request):
-#     if request.method == 'GET':
-#         posts = Post.objects.filter(status=True)
-#         serializer = PostSerializer(posts, many=True)
-#         return Response(serializer.data)
-#     elif request.method == 'POST':
-#         serializer = PostSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response(serializer.data)
-
-
 class PostList(APIView):
     """
     View a list of all posts and creating new post.
@@ -29,7 +16,7 @@ class PostList(APIView):
     permission_classes = [IsAuthenticated]
     serializer_class = PostSerializer
 
-    def get(self, request, format=None):
+    def get(self, request):
         posts = Post.objects.filter(status=True)
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
@@ -41,18 +28,24 @@ class PostList(APIView):
         return Response(serializer.data)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-@permission_classes([IsAuthenticatedOrReadOnly])
-def post_detail(request, id):
-    post = get_object_or_404(Post, pk=id, status=True)
-    if request.method == 'GET':
-        serializer = PostSerializer(post)
+class PostDetail(APIView):
+
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+
+    def get(self, request, id):
+        post = get_object_or_404(Post, pk=id, status=True)
+        serializer = self.serializer_class(post)
         return Response(serializer.data)
-    elif request.method == 'PUT':
+
+    def put(self, request, id):
+        post = get_object_or_404(Post, pk=id, status=True)
         serializer = PostSerializer(post, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    elif request.method == 'DELETE':
+
+    def delete(self, request, id):
+        post = get_object_or_404(Post, pk=id, status=True)
         post.delete()
         return Response({'detail': 'item removed successfully'}, status=status.HTTP_204_NO_CONTENT)
