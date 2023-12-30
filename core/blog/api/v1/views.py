4fbc57585e1d3744285d385c1cc48b9ec8c03979
raya <fati.mansoory@gmail.com>
2023-12-30 21:33:26 +0330

    use RetrieveUpdateDestroyAPIView for PostDetail

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status, mixins
-from rest_framework.generics import ListCreateAPIView, GenericAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .serializers import PostSerializer
 from blog.models import Post
@@ -14,18 +14,9 @@ class PostList(ListCreateAPIView):
     queryset = Post.objects.filter(status=True)
 
 
-class PostDetail(GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+class PostDetail(RetrieveUpdateDestroyAPIView):
 
     permission_classes = [IsAuthenticated]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
     # lookup_field = 'id'
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
