582f9e7cd26c942aeae3facadf08826a6c44cffe
raya <fati.mansoory@gmail.com>
2023-12-31 19:22:42 +0330

    switch to ModelViewSet instead of ViewSets

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -7,28 +7,7 @@ from .serializers import PostSerializer
 from blog.models import Post
 
 
-class PostViewSet(viewsets.ViewSet):
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
-
-    def list(self, request):
-        serializer = self.serializer_class(self.queryset, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        post = get_object_or_404(self.queryset, pk=pk)
-        serializer = self.serializer_class(post)
-        return Response(serializer.data)
-
-    def create(self, request):
-        pass
-
-    def update(self, request, pk=None):
-        pass
-
-    def partial_update(self, request, pk=None):
-        pass
-
-    def destroy(self, request, pk=None):
-        pass
