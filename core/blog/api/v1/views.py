b4787e1af0aa6727708028db972d1cebcceb1635
raya <fati.mansoory@gmail.com>
2023-12-31 20:23:40 +0330

    https://www.django-rest-framework.org/api-guide/routers/\#routing-for-extra-actions

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status, mixins, viewsets
+from rest_framework.decorators import action
 
 from .serializers import CategorySerializer, PostSerializer
 from blog.models import Post, Category
@@ -11,6 +12,10 @@ class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
+
+    @action(methods=['get'], detail=False)
+    def get_ok(self, request):
+        return Response({'detail': 'ok'})
 
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
