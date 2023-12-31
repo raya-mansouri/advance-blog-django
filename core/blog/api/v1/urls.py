4e017cae238dd6d5db3b25af392116e73e221669
raya <fati.mansoory@gmail.com>
2023-12-31 19:14:38 +0330

    switch to DefaultRouter

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -1,11 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
+from rest_framework.routers import DefaultRouter
 
 app_name = 'api-v1'
 
-urlpatterns = [
-    path('post/', PostViewSet.as_view({'get': 'list', 'post': 'create'}), name='post-list'),
-    path('post/<int:pk>/', PostViewSet.as_view({'get': 'retrieve', 'put': 'update',
-         'patch': 'partial_update', 'delete': 'destroy'}), name='post-detail'),
+router = DefaultRouter()
+router.register('post', PostViewSet, basename='post')
+urlpatterns = router.urls
 
-]
+# urlpatterns = [
+#     path(
+#         'post/', PostViewSet.as_view({'get': 'list', 'post': 'create'}), name='post-list'),
+#     path('post/<int:pk>/', PostViewSet.as_view({'get': 'retrieve', 'put': 'update',
+#          'patch': 'partial_update', 'delete': 'destroy'}), name='post-detail'),
+# ]
