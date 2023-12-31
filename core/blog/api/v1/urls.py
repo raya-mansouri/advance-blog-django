ee7839ff8edcb711ef02c4d46f3b10f020856e77
raya <fati.mansoory@gmail.com>
2023-12-31 18:11:34 +0330

    replace Generic views structure with ViewSets

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -4,7 +4,8 @@ from .views import *
 app_name = 'api-v1'
 
 urlpatterns = [
-    path('post/',PostList.as_view(), name='post-list'),
-    path('post/<int:pk>/',PostDetail.as_view(), name='post-detail'),
+    path('post/', PostViewSet.as_view({'get': 'list', 'post': 'create'}), name='post-list'),
+    path('post/<int:pk>/', PostViewSet.as_view({'get': 'retrieve', 'put': 'update',
+         'patch': 'partial_update', 'delete': 'destroy'}), name='post-detail'),
 
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,22 +1,34 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import status, mixins
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import status, mixins, viewsets
 
 from .serializers import PostSerializer
 from blog.models import Post
 
 
-class PostList(ListCreateAPIView):
+class PostViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
 
+    def list(self, request):
+        serializer = self.serializer_class(self.queryset, many=True)
+        return Response(serializer.data)
 
-class PostDetail(RetrieveUpdateDestroyAPIView):
+    def retrieve(self, request, pk=None):
+        post = get_object_or_404(self.queryset, pk=pk)
+        serializer = self.serializer_class(post)
+        return Response(serializer.data)
 
-    permission_classes = [IsAuthenticated]
-    serializer_class = PostSerializer
-    queryset = Post.objects.filter(status=True)
-    # lookup_field = 'id'
+    def create(self, request):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
