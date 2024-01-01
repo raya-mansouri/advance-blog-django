13a9a3bfa353bde922332ef336d8f9bbe6fc538c
raya <fati.mansoory@gmail.com>
2024-01-01 11:55:12 +0330

    use inner functoin in serializers.py

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -12,11 +12,17 @@ class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(
         source='get_absolute_api_url', read_only=True)
+    absolute_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = ['id', 'author', 'title', 'content', 'snippet', 'category',
-                  'status', 'relative_url', 'created_date', 'published_date']
+                  'status', 'relative_url', 'absolute_url', 'created_date', 'published_date']
+
+    # same name as line 15    
+    def get_absolute_url(self,obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
 
 
 class CategorySerializer(serializers.ModelSerializer):
