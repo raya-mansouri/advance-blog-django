78710251a5316660fa21997ca1b52ea6ba09c2dc
raya <fati.mansoory@gmail.com>
2024-01-01 11:18:59 +0330

    link serializers with function in the models.py

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -2,16 +2,21 @@ from blog.models import Post, Category
 from rest_framework import serializers
 
 # class PostSerializer(serializers.Serializer):
-    # id = serializers.IntegerField()
-    # author = serializers.EmailField()
-    # title = serializers.CharField(max_length=200)
+# id = serializers.IntegerField()
+# author = serializers.EmailField()
+# title = serializers.CharField(max_length=200)
 #     created_date = serializers.DateTimeField()
 
+
 class PostSerializer(serializers.ModelSerializer):
+    snippet = serializers.ReadOnlyField(source='get_snippet')
+    relative_url = serializers.URLField(
+        source='get_absolute_api_url', read_only=True)
 
     class Meta:
         model = Post
-        fields = ['id', 'author', 'title', 'status', 'created_date','published_date']
+        fields = ['id', 'author', 'title', 'content', 'snippet', 'category',
+                  'status', 'relative_url', 'created_date', 'published_date']
 
 
 class CategorySerializer(serializers.ModelSerializer):
--- core/blog/models.py
+++ core/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -19,6 +20,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_snippet(self):
+        return self.content[:10]
+    
+    def get_absolute_api_url(self):
+        return reverse("blog:api-v1:post-detail", kwargs={"pk": self.pk})
+    
 
 
 class Category(models.Model):
