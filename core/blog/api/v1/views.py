6111ef23abc4f2dbb77f142e4e118fed17869bbe
raya <fati.mansoory@gmail.com>
2024-01-03 18:06:28 +0330

    add OrderingFilter https://www.django-rest-framework.org/api-guide/filtering/\#orderingfilter

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status, mixins, viewsets
 from rest_framework.decorators import action
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from .serializers import CategorySerializer, PostSerializer
 from blog.models import Post, Category
@@ -14,9 +14,10 @@ class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['category', 'author', 'status']
     search_fields = ['title', 'content']
+    ordering_fields = ['published_date']
 
     @action(methods=['get'], detail=False)
     def get_ok(self, request):
