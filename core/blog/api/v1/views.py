9bb05bcea9d632ac498caf2e65316ed20c869baf
raya <fati.mansoory@gmail.com>
2024-01-03 17:53:56 +0330

    add django-filters https://www.django-rest-framework.org/api-guide/filtering/

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 from rest_framework.response import Response
 from rest_framework import status, mixins, viewsets
 from rest_framework.decorators import action
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .serializers import CategorySerializer, PostSerializer
 from blog.models import Post, Category
@@ -12,6 +13,8 @@ class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category', 'author', 'status']
 
     @action(methods=['get'], detail=False)
     def get_ok(self, request):
--- core/core/settings.py
+++ core/core/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_filters',
+    
     'accounts',
     'blog',
 ]
